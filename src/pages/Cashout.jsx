898e48246143a16bd72bf23954509e12d6c11f08
Beyza Keser <beyzaaakeser@gmail.com>
2024-10-19 16:28:29 +0300

    cashout updated

--- src/pages/Cashout.jsx
+++ src/pages/Cashout.jsx
@@ -1,121 +1,161 @@
-import React, {useContext, useEffect, useState} from 'react';
-import {CartContext} from "../redux/CartContext.jsx";
-import {Link, Navigate, useNavigate} from "react-router-dom";
-import {auth, db} from "../firebase/index.js";
-import Navbar from "../components/Navbar.jsx";
-import {doc, getFirestore, onSnapshot, setDoc} from 'firebase/firestore';
-import {getAuth, onAuthStateChanged} from "firebase/auth";
+import React, { useContext, useEffect, useState } from 'react';
+import { CartContext } from '../redux/CartContext.jsx';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
+import { auth, db } from '../firebase/index.js';
+import Navbar from '../components/Navbar.jsx';
+import { doc, getFirestore, onSnapshot, setDoc } from 'firebase/firestore';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
-const Cashout = ({user}) => {
-    const navigate = useNavigate();
-    const {shoppingCart, totalPrice, totalQty, dispatch} = useContext(CartContext);
+const Cashout = ({ user }) => {
+  const navigate = useNavigate();
+  const { shoppingCart, totalPrice, totalQty, dispatch } =
+    useContext(CartContext);
 
-    // State tanımlamaları
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [cell, setCell] = useState('');
-    const [address, setAddress] = useState('');
-    const [error, setError] = useState('');
-    const [successMsg, setSuccessMsg] = useState('');
+  // State tanımlamaları
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [cell, setCell] = useState('');
+  const [address, setAddress] = useState('');
+  const [error, setError] = useState('');
+  const [successMsg, setSuccessMsg] = useState('');
 
-    useEffect(() => {
-        const unsubscribeAuth = onAuthStateChanged(auth, (user) => {
-            if (user) {
-                const userRef = doc(db, 'SignedUpUsersData', user.uid); // Kullanıcı verisi için referans al
-                const unsubscribeUserData = onSnapshot(userRef, (snapshot) => { // Gerçek zamanlı güncellemeler için
-                    if (snapshot.exists()) {
-                        setName(snapshot.data().name);
-                        setEmail(snapshot.data().email);
-                    } else {
-                        console.error('Kullanıcı verisi mevcut değil');
-                    }
-                });
-
-                // Bileşen unmounted olduğunda kullanıcı veri dinleyicisini temizle
-                return () => unsubscribeUserData();
-            } else {
-                navigate('/login');
-            }
+  useEffect(() => {
+    const unsubscribeAuth = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const userRef = doc(db, 'SignedUpUsersData', user.uid); // Kullanıcı verisi için referans al
+        const unsubscribeUserData = onSnapshot(userRef, (snapshot) => {
+          // Gerçek zamanlı güncellemeler için
+          if (snapshot.exists()) {
+            setName(snapshot.data().name);
+            setEmail(snapshot.data().email);
+          } else {
+            console.error('Kullanıcı verisi mevcut değil');
+          }
         });
 
-        // Auth dinleyicisini temizle
-        return () => unsubscribeAuth();
-    }, []);
+        // Bileşen unmounted olduğunda kullanıcı veri dinleyicisini temizle
+        return () => unsubscribeUserData();
+      } else {
+        navigate('/login');
+      }
+    });
 
-    const cashoutSubmit = async (e) => {
-        e.preventDefault();
+    // Auth dinleyicisini temizle
+    return () => unsubscribeAuth();
+  }, []);
 
-        const user = auth.currentUser; // Şu anki kullanıcıyı al
-        if (user) {
-            const date = new Date();
-            const time = date.getTime();
-            const docRef = doc(db, 'Buyer-info', user.uid + '_' + time); // Belge referansı oluştur
+  const cashoutSubmit = async (e) => {
+    e.preventDefault();
 
-            try {
-                await setDoc(docRef, {
-                    BuyerName: name,
-                    BuyerEmail: email,
-                    BuyerCell: cell,
-                    BuyerAddress: address,
-                    BuyerPayment: totalPrice,
-                    BuyerQuantity: totalQty
-                });
+    const user = auth.currentUser; // Şu anki kullanıcıyı al
+    if (user) {
+      const date = new Date();
+      const time = date.getTime();
+      const docRef = doc(db, 'Buyer-info', user.uid + '_' + time); // Belge referansı oluştur
 
-                // Form alanlarını sıfırla ve dispatch işlemi yap
-                setCell('');
-                setAddress('');
-                dispatch({type: 'EMPTY'});
-                setSuccessMsg('Siparişiniz başarıyla verilmiştir. 3 saniye sonra anasayfaya yönlendirileceksiniz.');
+      try {
+        await setDoc(docRef, {
+          BuyerName: name,
+          BuyerEmail: email,
+          BuyerCell: cell,
+          BuyerAddress: address,
+          BuyerPayment: totalPrice,
+          BuyerQuantity: totalQty,
+        });
 
-                // 5 saniye sonra anasayfaya yönlendir
-                setTimeout(() => {
-                    navigate('/');
-                }, 3000);
-            } catch (err) {
-                setError(err.message);
-            }
-        }
-    };
+        // Form alanlarını sıfırla ve dispatch işlemi yap
+        setCell('');
+        setAddress('');
+        dispatch({ type: 'EMPTY' });
+        setSuccessMsg(
+          'Siparişiniz başarıyla verilmiştir. 3 saniye sonra anasayfaya yönlendirileceksiniz.'
+        );
 
-    return (
-        <>
-            <Navbar user={user}/>
+        // 5 saniye sonra anasayfaya yönlendir
+        setTimeout(() => {
+          navigate('/');
+        }, 3000);
+      } catch (err) {
+        setError(err.message);
+      }
+    }
+  };
 
-            <div className='container cashoutContainer'>
-                <h2>Cash out Details</h2>
-                {successMsg && <div className='success-msg'>{successMsg}</div>}
-                <form autoComplete="off" className='form-group' onSubmit={cashoutSubmit}>
-                    <label htmlFor="name">Name</label>
-                    <input type="text" className='form-control' required
-                           value={name} disabled/>
+  return (
+    <>
+      <Navbar user={user} />
 
-                    <label htmlFor="email">Email</label>
-                    <input type="email" className='form-control' required
-                           value={email} disabled/>
+      <div className="container cashoutContainer">
+        <h2>Cash out Details</h2>
+        {successMsg && <div className="success-msg">{successMsg}</div>}
+        <form
+          autoComplete="off"
+          className="form-group"
+          onSubmit={cashoutSubmit}
+        >
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            className="form-control"
+            required
+            value={name}
+            disabled
+          />
 
-                    <label htmlFor="Cell No">Cell No</label>
-                    <input type="number" className='form-control' required
-                           onChange={(e) => setCell(e.target.value)} value={cell}
-                           placeholder='örneğin 03123456789'/>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            className="form-control"
+            required
+            value={email}
+            disabled
+          />
 
-                    <label htmlFor="Delivery Address">Delivery Address</label>
-                    <input type="text" className='form-control' required
-                           onChange={(e) => setAddress(e.target.value)} value={address}/>
+          <label htmlFor="Cell No">Cell No</label>
+          <input
+            type="number"
+            className="form-control"
+            required
+            onChange={(e) => setCell(e.target.value)}
+            value={cell}
+            placeholder="Exp 03123456789"
+          />
 
-                    <label htmlFor="Price To Pay">Price To Pay</label>
-                    <input type="number" className='form-control' required
-                           value={Number(totalPrice).toFixed(2)} disabled/>
+          <label htmlFor="Delivery Address">Delivery Address</label>
+          <input
+            type="text"
+            className="form-control"
+            required
+            onChange={(e) => setAddress(e.target.value)}
+            value={address}
+          />
 
-                    <label htmlFor="Total No of Products">Total No of Products</label>
-                    <input type="number" className='form-control' required
-                           value={totalQty} disabled/>
+          <label htmlFor="Price To Pay">Price To Pay</label>
+          <input
+            type="number"
+            className="form-control"
+            required
+            value={Number(totalPrice).toFixed(2)}
+            disabled
+          />
 
-                    <button type="submit" className='btn btn-success btn-md mybtn'>SUBMIT</button>
-                </form>
-                {error && <span className='error-msg'>{error}</span>}
-            </div>
-        </>
-    )
+          <label htmlFor="Total No of Products">Total No of Products</label>
+          <input
+            type="number"
+            className="form-control"
+            required
+            value={totalQty}
+            disabled
+          />
+
+          <button type="submit" className="btn btn-success btn-md mybtn">
+            SUBMIT
+          </button>
+        </form>
+        {error && <span className="error-msg">{error}</span>}
+      </div>
+    </>
+  );
 };
 
 export default Cashout;
